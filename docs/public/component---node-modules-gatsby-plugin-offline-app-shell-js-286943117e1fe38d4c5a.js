42f91610ddce9e7af6bf7dc3ef40a866424e8600
Kris <jurcickristijan96@gmail.com>
2019-11-16 13:59:43 +0100

    rm public

--- docs/public/component---node-modules-gatsby-plugin-offline-app-shell-js-286943117e1fe38d4c5a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{199:function(e,t,n){"use strict";var u=n(15);t.__esModule=!0,t.default=void 0;var o=u(n(71)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
-//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-286943117e1fe38d4c5a.js.map
--- public/styles-299b0c351ce39c291477.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{159:function(n,o,w){},160:function(n,o,w){},204:function(n,o,w){}}]);
-//# sourceMappingURL=styles-299b0c351ce39c291477.js.map
